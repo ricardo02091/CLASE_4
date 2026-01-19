3f0e90fac54b8d2ffbe5d4134385105f7803bdc7
RichardMax <123@gmail.com>
2026-01-18 20:54:39 -0500

    segundo commit

--- Ejer_3_Com_numero.py
+++ Ejer_3_Com_numero.py
@@ -9,3 +9,5 @@ print(f"{numero1} > {numero2}: {numero1 > numero2}")
 print(f"{numero1} < {numero2}: {numero1 < numero2}")
 print(f"{numero1} >= {numero2}: {numero1 >= numero2}")
 print(f"{numero1} <= {numero2}: {numero1 <= numero2}")
+
+#asdfasdfas
